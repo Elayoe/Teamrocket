82fcfce8de30cf9c0b07d44573939846b4f93b5e
Lapras <ASehr@hotmail.de>
2013-09-23 15:54:03 +0200

    strange commit

---
+++ TaskManagerWeb04/src/com/taskmanager4/TaskManagerWeb04Servlet.java
@@ -0,0 +1,13 @@
+package com.taskmanager4;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TaskManagerWeb04Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
